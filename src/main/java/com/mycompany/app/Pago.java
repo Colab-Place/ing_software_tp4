fab48e9c2ac9796853abedf6a1ce95c238b71056
starmickey <micaelaestrellamorales@gmail.com>
2023-06-10 16:39:39 -0300

    Create Pago.java

--- src/main/java/com/mycompany/app/Pago.java
+++ src/main/java/com/mycompany/app/Pago.java
@@ -1,15 +1,41 @@
 package com.mycompany.app;
 
 public class Pago {
+    private final String id;
     private Cliente cliente;
+    private double monto;
+
 
 
     public Pago() {
+        this.id = "99999999";
     }
 
-    public Pago(Cliente cliente) {
+
+
+    public Pago(String id, Cliente cliente, double monto) {
+        this.id = id;
         this.cliente = cliente;
+        this.monto = monto;
     }
+
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public double getMonto() {
+        return monto;
+    }
+
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+
 
     public Cliente getCliente() {
         return cliente;
