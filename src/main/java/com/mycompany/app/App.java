36c56049672a1a86d5bb5d14d7d0a05e7962e58e
starmickey <micaelaestrellamorales@gmail.com>
2023-06-10 11:31:16 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Running!" );
+
+        
+
+    }
+
+
+}
